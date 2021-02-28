f57595429d737cf4d8632a695f11c7b403780b9b
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2021-03-01 00:17:09 +0600

    Base_url

---
+++ src/api.js
@@ -0,0 +1,30 @@
+// Base url of the api
+const base_url = "https://api.rawg.io/api/";
+
+const getCurrentMonth = () => {
+  const month = new Date.getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  }
+  return month;
+};
+
+const getCurrentDay = () => {
+  const day = new Date.getDate();
+  if (day < 10) {
+    return `0${day}`;
+  }
+  return day;
+};
+
+const currentYear = new Date.getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+const popularGames = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+const popularGamesUrl = () => `${base_url}${popularGames}`;
